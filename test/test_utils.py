491ebe7e32f2feb01ab0fa4dc51a8f4121c88ab4
faruk-ahmad <faruk.csebrur@gmail.com>
2020-04-30 01:46:37 +0900

    added TestAddition class

--- test/test_utils.py
+++ test/test_utils.py
@@ -2,7 +2,20 @@ import unittest
 import sys
 sys.path.append('..')
 
-from myapp.utils import division
+from myapp.utils import division, addition
+
+class TestAddition(unittest.TestCase):
+    def setup(self):
+        pass
+
+    def tear_down(self):
+        pass
+
+    def test_additions(self):
+        self.assertEqual(addition(10, 5), 15, 'Failed to add 10 and 5')
+        self.assertEqual(addition(0, 4), 4, 'Failed to add 0 and 4')
+        self.assertEqual(addition(4, 0), 4, 'Failed to add 4 and 0')
+        self.assertEqual(addition(-1, 0), -1, 'Failed to add -1 and 0')
 
 
 class TestDivision(unittest.TestCase):
